8988c820dd57c181070e7b844f5a13a8f02e1607
nofprog <k.jin3217@gmail.com>
2021-04-27 16:21:51 +0900

    debug

--- HWS_Project18/ViewController.swift
+++ HWS_Project18/ViewController.swift
@@ -11,8 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        print(1,2,3,4,5, separator: "-")
+        //1-2-3-4-5
+
+        
+        for i in 1...100 {
+            print(i)
+        }
+        /*
+         1
+         2
+         3
+         4
+         (lldb) p i
+         (Int) $R0 = 4
+         */
+            }
 
 
 }
